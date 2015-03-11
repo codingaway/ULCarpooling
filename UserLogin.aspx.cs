fcefe02a34a9bf49f062ec6a1514fce5e873da2a
Abdul Halim <halim.bepari@gmail.com>
2015-03-11 12:45:36 +0000

    same changes

---
+++ UserLogin.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default6 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ASP.login_ascx login = (ASP.login_ascx)LoadControl("~/Login.ascx");
+        plhLogin.Controls.Add(login);
+    }
+}
