0389c480524e1551aabee46beb4ba85d8a2e80eb
JoshuaHeth <13061283@studentmail.ul.ie>
2015-03-16 13:08:14 +0000

    Josh's User Profile changes

--- JoshsTemp.aspx.cs
+++ JoshsTemp.aspx.cs
@@ -5,48 +5,76 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Drawing;
 
 public partial class JoshsTemp : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        
         if (!IsPostBack)
         {
             //Code to get User_ID from cookie
 
             //Need a command to load users image from DB and display to profileImage
+            //profileImage.ImageUrl = "~/DisplayImg.ashx?id=" + 3; //User_ID from cookie needed
 
-            //Need commands to load name, email, phone and password from DB and display to txtName, txtEmail, txtPhone and txtPassword
+            //Need commands to load name, email and phone from DB and display to txtName, txtEmail, txtPhone
 
             SqlConnection sql = new SqlConnection("Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\Loaner\\ULCarpooling\\App_Data\\carpooling_db.mdf;Integrated Security=True;");
-            SqlCommand cmd = new SqlCommand();
-            SqlDataReader reader;
+            //SqlCommand cmd = new SqlCommand();
 
-            cmd.CommandText = "Select * from users Where User_ID = 3";
-            cmd.CommandType = System.Data.CommandType.Text;
-            cmd.Connection = sql;
-
-            sql.Open();
-
-            reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
+            using(SqlCommand cmd = new SqlCommand("Select * from users Where User_ID = 3", sql))
             {
-                while (reader.Read())
-                {
-                    txtName.Text = reader.GetString(1) + " " + reader.GetString(2);
-                    txtEmail.Text = reader.GetString(3);
-                    txtPhone.Text = reader.GetString(4);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = sql;
 
+                sql.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows){
+                    while (reader.Read()){
+                        txtName.Text = reader.GetString(1) + " " + reader.GetString(2);
+                        txtEmail.Text = reader.GetString(3);
+                        txtPhone.Text = reader.GetString(4);
+                    
+                        if (reader["profile_pic"] == System.DBNull.Value){
+                            profileImage.ImageUrl = "~/Images/emptyProfileImage.png";
+                        }
+                        else{
+                            //profileImage.ImageUrl = "~/DisplayImg.ashx?id=" + 3;
+                        }
+                    }
                 }
+                else{
+                    Response.Write("No data found");
+                }
+                reader.Close();
             }
-            else
-            {
-                Response.Write("No data found");
-            }
-            reader.Close();
-            sql.Close();
 
+            using(SqlCommand cmd = new SqlCommand("SELECT password FROM secret WHERE user_id = 3", sql))
+            {
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = sql;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if(reader.Read()){
+                    string pw = "";
+                    for (int i = 0; i < (reader["password"].ToString()).Length; i++){
+                        pw += "*";
+                    }
+                    txtPassword.Text = pw;
+                } 
+                else{
+                    txtPassword.Text = "Error loading password";
+                }
+                reader.Close();
+            }
+            
+            
 
             //Need a command to load feedback from DB to feedbackImage
 
@@ -55,11 +83,19 @@ public partial class JoshsTemp : System.Web.UI.Page
             //Need a command to load Notifications from DB
 
             //Need a command to load Trip Histories from DB
+
+            //reader.Close();
+            sql.Close();
         }
     }
 
     protected void editDetails_Click(object sender, EventArgs e)
     {
         Response.Redirect("JoshsEditProfile.aspx");
+    }
+
+    protected void btnChangePW_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("JoshsChangePW.aspx");
     }
 }
